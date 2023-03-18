ce3e36abb53a52718e0604102af18759187e8483
Chris Markiewicz <effigies@gmail.com>
2023-03-18 11:41:26 -0400

    RF: Abstract checks

--- miniqc/__main__.py
+++ miniqc/__main__.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 import typing as ty
+from dataclasses import dataclass
 from enum import Enum
 from math import prod
 from pathlib import Path
@@ -9,15 +10,58 @@ from pathlib import Path
 import nibabel as nb
 import typer
 
+T = ty.TypeVar('T')
+
+
+@dataclass
+class CheckList(ty.Generic[T]):
+    """Object that pairs a file loader with boolean checks on the loaded file"""
+
+    loader: ty.Callable[[os.PathLike], T]
+    checks: list[ty.Callable[[T], bool]]
+
+
+def nifti_load(path: os.PathLike) -> nb.Nifti1Image:
+    """Load NIfTI-1 or NIfTI-2 (including CIFTI-2) images"""
+    try:
+        return nb.Nifti1Image.from_filename(path)
+    except Exception:
+        return nb.Nifti2Image.from_filename(path)
+
+
+def nifti_fullsize(img: nb.Nifti1Image) -> bool:
+    """Verify NIfTI image is seekable to expected length"""
+    expected_size = int(
+        img.header['vox_offset']
+        + (img.header['bitpix'] // 8) * prod(img.shape)
+    )
+
+    # Nudge type checker
+    assert isinstance(img.dataobj, nb.arrayproxy.ArrayProxy)
+    with img.dataobj._get_fileobj() as fobj:
+        fobj.seek(expected_size)
+
+    return True
+
+
+# Regex patterns paired with checklists to verify on file
+# To avoid multiple reads, only the first matching check is applied to each
+# file
+CHECKS: list[tuple[re.Pattern[str], CheckList]] = [
+    (
+        re.compile(r'\.nii(.gz)?$'),
+        CheckList(loader=nifti_load, checks=[nifti_fullsize]),
+    ),
+]
+
+app = typer.Typer()
+
 
 class AnalysisLevel(Enum):
     run = 'run'
     session = 'session'
     participant = 'participant'
     dataset = 'dataset'
-
-
-app = typer.Typer()
 
 
 @app.command()
@@ -37,35 +81,26 @@ def main(
         dirs[:] = [d for d in dirs if not d[0] == '.']
         root_path = Path(root)
         for file in files:
-            if re.search(r'\.nii(.gz)?$', file):
-                path = root_path / file
-                try:
+            for pattern, checklist in CHECKS:
+                if re.search(pattern, file):
+                    path = root_path / file
                     try:
-                        img = nb.Nifti1Image.from_filename(path)
-                    except Exception:
-                        img = nb.Nifti2Image.from_filename(path)
-                    expected_size = int(
-                        img.header['vox_offset']
-                        + (img.header['bitpix'] // 8) * prod(img.shape)
-                    )
-
-                    # Nudge type checker
-                    assert isinstance(img.dataobj, nb.arrayproxy.ArrayProxy)
-                    with img.dataobj._get_fileobj() as fobj:
-                        fobj.seek(expected_size)
-
-                except Exception as e:
-                    if allow_dangling_links and isinstance(
-                        e, FileNotFoundError
-                    ):
-                        continue
-                    errors.append(
-                        (
-                            str(path.relative_to(bids_dir)),
-                            type(e).__name__,
-                            str(e),
-                        )
-                    )
+                        fileobj = checklist.loader(path)
+                        for check in checklist.checks:
+                            if not check(fileobj):
+                                raise ValueError(check.__doc__)
+                    except Exception as e:
+                        if not allow_dangling_links or not isinstance(
+                            e, FileNotFoundError
+                        ):
+                            errors.append(
+                                (
+                                    str(path.relative_to(bids_dir)),
+                                    type(e).__name__,
+                                    str(e),
+                                )
+                            )
+                    break
     print(json.dumps(errors, indent=2))
     typer.Exit(len(errors))
 
